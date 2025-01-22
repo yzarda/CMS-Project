606e3ccdec7cee6a5dcd1f0632fcbc270b784477
yzarda <ardayildiz.business@gmail.com>
2025-01-22 14:19:40 +0100

    Update Vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,38 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
-  
+  const cards = [
+    {
+      title: "Lorem ipsum dolor sit amet",
+      content:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    },
+    {
+      title: "Consetetur sadipscing elitr",
+      content:
+        "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    },
+    {
+      title: "Sed diam nonumy eirmod tempor",
+      content:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    },
+  ];
 
   return (
-    <>
-    <h1 className="font-bold">super</h1>
-      <Card />
-      <Card />
-    </>
-  )
+    <div className="app">
+      <h1>HTL Dornbirn</h1>
+      <div className="button-container">
+        <button className="button">Filter Öffnen</button>
+        <button className="button">Hausübung Hinzufügen</button>
+      </div>
+      <div className="card-container">
+        {cards.map((card, index) => (
+          <Card key={index} title={card.title} content={card.content} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import "./Card.css";
 
-export default function Card() {
-  return (
-    <div>Card</div>
-)
+interface CardProps {
+  title: string;
+  content: string;
 }
+
+function Card({ title, content }: CardProps) {
+  return (
+    <div className="card">
+      <div className="card-header">{title}</div>
+      <div className="card-content">{content}</div>
+    </div>
+  );
+}
+
+export default Card;
